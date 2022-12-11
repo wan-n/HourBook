9dd44b4082635c6dec4143f90eef677a14690ecc
soowan kim <tndhks1916@naver.com>
2022-12-11 21:07:03 +0900

    mockup sa

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 makeScrollAnimation();
-showTitleMotion();
 
-
+makeTitleSA();
+makeIntroSA();
 
 
 
@@ -22,6 +22,7 @@ function makeScrollAnimation(){
                 timer = null;
 
                 //메서드
+                makeIntroSA();
 
                 lastScrollY = window.scrollY;
             }, 100);
@@ -31,30 +32,78 @@ function makeScrollAnimation(){
 
 
 
-function showTitleMotion(){
+function makeTitleSA(){
     const textBox = document.querySelector('.title-box');
     const logo = document.querySelector('.title-logo');
     const title = document.querySelector('h1');
     const arrow = document.querySelector('.scroll-arrow');
 
-    textBox.style.transition = '.8s';
-    logo.style.transition = '.5s';
-    title.style.transition = '.5s';
-    arrow.style.transition = '1s';
-    textBox.classList.remove('opacity-zero');
-    textBox.classList.remove('to-bottom');
+    if(textBox.getBoundingClientRect().top - window.innerHeight < 0){
+        textBox.style.transition = '.8s';
+        logo.style.transition = '.5s';
+        title.style.transition = '.5s';
+        arrow.style.transition = '1s';
 
-    setTimeout(() => {
-        logo.classList.add('opacity-zero');
-    }, 800);
-
-    setTimeout(() => {
-        logo.classList.add('hide');
-        title.style.height = 'auto';
-        title.classList.remove('opacity-zero');
-    }, 1300);
-
-    setTimeout(() => {
-        arrow.classList.remove('opacity-zero');
-    }, 1800);
+        textBox.classList.remove('opacity-zero');
+        textBox.classList.remove('to-bottom');
+    
+        setTimeout(() => {
+            logo.classList.add('opacity-zero');
+        }, 800);
+    
+        setTimeout(() => {
+            logo.classList.add('hide');
+            title.style.height = 'auto';
+            title.classList.remove('opacity-zero');
+        }, 1300);
+    
+        setTimeout(() => {
+            arrow.classList.remove('opacity-zero');
+        }, 1800);
+    }
 }
+
+
+
+function makeIntroSA(){
+    const container = document.querySelector('.inner-wrapper.intro');
+    const slideContainer = document.querySelector('.screen-carousel-container');
+    const frame = document.querySelector('.screen-carousel-frame');
+    const moveLength = document.querySelector('.screen-carousel-frame').clientWidth;
+    const contentsWrapper = document.querySelector('.screen-carousel-contents');
+    const count = 4;
+
+
+    if(container.getBoundingClientRect().top - window.innerHeight < 0){
+        container.style.transition = '.8s';
+
+        container.classList.remove('opacity-zero');
+        setTimeout(slideContents(contentsWrapper, moveLength, count, 800), 800);
+
+        setTimeout(() => {
+            //이미지 뒤로 펼치기
+            
+        }, 800*count);
+    }
+
+
+}
+
+
+
+function slideContents(wrapper, moveLength, count, sec){
+    const contentsWrapper = wrapper;
+    let nowIdx = 1;  //현재 인덱스
+
+    contentsWrapper.style.transition = '.5s'
+
+    let abc = setInterval(() => {
+        contentsWrapper.style.transform = `translateX(-${moveLength*nowIdx++}px)`;
+
+        if(nowIdx === count){
+            clearInterval(abc);
+        }
+    }, sec);
+}
+
+    
